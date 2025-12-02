e39667ef50cd399ed220d1d3ec45cb4f12d7c28d
surajarya1999 <surajarya460@gmail.com>
2025-12-02 09:18:25 +0530

    backend update

--- SERVERcar/app.js
+++ SERVERcar/app.js
@@ -19,13 +19,12 @@ app.use((req, res, next) => {
 
 app.use(cors({
   origin: [
-    "http://localhost:5173",
-    "https://car-rental-with-suraj.netlify.app"
+    "https://car-rental-with-suraj.netlify.app",
+    "http://localhost:5173"
   ],
-  credentials: true,
-  methods: ["GET", "POST", "PUT", "DELETE"],
-  allowedHeaders: ["Content-Type", "Authorization"]
+  credentials: true
 }));
+
 
 
 app.use(express.urlencoded({ extended: true }));
